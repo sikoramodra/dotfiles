4130a214e1920f99dbe946d0e58315de75c16a97
sikoramodra <wojciechmodro@gmail.com>
2025-02-04 11:40:58 +0100

    move to hyprpanel

---
+++ .bash_profile
@@ -0,0 +1,9 @@
+#
+# ~/.bash_profile
+#
+
+if [ "$(tty)" = "/dev/tty1" ]; then
+    exec Hyprland
+fi
+
+[[ -f ~/.bashrc ]] && . ~/.bashrc
--- .config/ags/lib/types/bar.d.ts
+++
@@ -1,9 +0,0 @@
-import { Variable } from "types/variable";
-
-export type Child = {
-    component: Box<Gtk.Widget, unknown>;
-    isVisible?: boolean;
-    isVis?: Variable<boolean>;
-    boxClass: string;
-    props: ButtonProps;
-};
