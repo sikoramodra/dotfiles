84aa780cd13ba5e7e8254ff69f5a769d186753f0
Wojciech Modro <wojciechmodro@gmail.com>
2026-06-03 11:52:20 +0200

    update install script

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ if [ "$STAGE" == 1 ]; then
     omarchy-install-terminal kitty
     omarchy-install-zed
     omarchy-install-browser brave-origin
-    sudo pacman -S stow fish adobe-source-code-pro-fonts tela-circle-icon-theme-blue
+    sudo pacman -S --noconfirm stow fish adobe-source-code-pro-fonts tela-circle-icon-theme-blue
 
     git clone https://github.com/sikoramodra/dotfiles.git ~/.dotfiles
     cd ~/.dotfiles
@@ -25,15 +25,16 @@ fi
 
 # Left-Top corner > System > Logout
 # CTRL ALT F2
+# ./install.sh 2
 
 if [ "$STAGE" == 2 ]; then
     rm -f ~/.local/share/applications/{Alacritty,imv,typora}.desktop
-    rm -f ~/.config/{chromium-flags.conf,mimeapps.list,omarchy.ttf,starship.toml,user-dirs.dirs,xdg-terminals.list}
+    rm -f ~/.config/{chromium-flags.conf,mimeapps.list,omarchy.ttf,starship.toml,user-dirs.dirs,xdg-terminals.list,brave-origin-beta-flags.conf}
     rm -f ~/.bashrc
 
     for dir in autostart environment.d fastfetch fcitx5 fontconfig foot git \
-        gtk-3.0 hypr hyprland-preview-share-picker imv kitty mise nvim \
-        omarchy qalculate swayosd uwsm walker waybar wiremix wireplumber; do
+        gtk-3.0 hypr hyprland-preview-share-picker imv kitty mise nvim omarchy \
+        qalculate swayosd uwsm walker waybar wiremix wireplumber zed; do
         rm -rf "$HOME/.config/$dir"
     done
 
@@ -50,35 +51,33 @@ if [ "$STAGE" == 2 ]; then
 fi
 
 # Reboot
+# ./install.sh 3
 
 if [ "$STAGE" == 3 ]; then
     omarchy-default-browser brave-origin
-    # manual disable brave://flags/#enable-webrtc-allow-input-volume-adjustment
-    # sync - have sync code - >I >localsend
 
     omarchy-remove-preinstalls
-    sudo pacman -S cliamp pinta obs-studio kdenlive lazydocker
-    # (https://github.com/bjarneo/cliamp/blob/main/docs/youtube-music.md)
-
+    sudo pacman -S --noconfirm cliamp lazydocker pinta obs-studio kdenlive gimp gparted xorg-xhost 7zip cava figlet
+    # https://github.com/bjarneo/cliamp/blob/main/docs/youtube-music.md
     omarchy-tui-install Docker lazydocker float file:///usr/share/icons/Tela-circle-blue/scalable/apps/docker.svg
 
-    sudo pacman -S gimp gparted xorg-xhost 7zip cava figlet
-    # sudo pacman -Rns alacritty
-
-    rm -rf .agents .claude .codex .pi Work
+    rm -rf ~/.agents ~/.claude ~/.codex ~/.pi ~/Work
 
     omarchy-pkg-aur-add breezex-cursor-theme
     gsettings set org.gnome.desktop.interface cursor-theme 'BreezeX-Dark'
-    # (dconf dump /org/gnome/desktop/interface)
+    # dconf dump /org/gnome/desktop/interface
 
     # [dev]
-    sudo pacman -S nginx tree-sitter-cli valkey postgresql act delve go go-tools pgformatter
+    sudo pacman -S --noconfirm nginx tree-sitter-cli valkey postgresql act delve go go-tools pgformatter
     omarchy-install-browser zen
     omarchy-pkg-aur-add beekeeper-studio-bin yaak-bin bruno-bin etcher-bina simplenote-electron-bin
-
-    nvim :MasonInstallAll :TSInstallAll
-
-    sudo pacman -Rns bruno-bin-debug simplenote-electron-bin-debug
-
-    # copy cliamp ytmusic secret
+    sudo pacman -Rns --noconfirm alacritty bruno-bin-debug simplenote-electron-bin-debug
 fi
+
+# Manual:
+# nvim :MasonInstallAll :TSInstallAll
+#
+# copy cliamp ytmusic secret
+#
+# brave://flags/#enable-webrtc-allow-input-volume-adjustment
+# sync - have sync code -> Localsend
